42246853c2176eba0a25599638839e2cce069956
Vepa Babayev <vepababayev03@gmail.com>
2025-09-17 15:51:09 +0200

    added HomePage

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:german_quiz_app/pages/HomePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomePage()
+    );
+  }
+}
